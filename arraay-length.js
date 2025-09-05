5b962c36762c3b688e0cb07235b7933115957c07
rjsuchy <rifatjahansuchy@gmail.com>
2025-09-05 09:53:48 +0600

    1st array commit

---
+++ arraay-length.js
@@ -0,0 +1,20 @@
+const numbers = [1,3,4,6,7,9,3,20];
+/*
+numbers[2] = 90
+console.log(numbers[2]);
+
+numbers.push(99,96,100,666
+);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);*/
+
+ /* numbers.shift();
+console.log(numbers);
+numbers.unshift(5);
+console.log(numbers);
+console.log(numbers.includes(96)); */
+
+console.log(numbers.includes(2));;
+console.log(numbers.indexOf(99));
---
+++ arraytask1.js
@@ -0,0 +1,10 @@
+const fruits = ["Aam", "Jaam", "Kathal", "Kola", "Lichu"];
+
+console.log(fruits);
+console.log("Total fruits:", fruits.length);
+
+
+
+console.log(fruits[2]);
+fruits[1] = "jambura";
+console.log(fruits);
