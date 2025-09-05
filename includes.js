2641729c4150f8e3af26b1a9db9e9c9136adbbfd
rjsuchy <rifatjahansuchy@gmail.com>
2025-09-05 12:13:58 +0600

    includes iddexof

---
+++ includes.js
@@ -0,0 +1,6 @@
+const Booklist = ["al quran", "Hadis book", "Siratunnabi","Machine learning" ];
+console.log(Booklist.includes("javascript"));
+console.log(Booklist.indexOf(99));
+
+console.log(Booklist.includes("al quran"));
+console.log(Booklist.indexOf("al quran"));
