ea0813e06311b1790b0e2ffef6049b53befce9db
rjsuchy <rifatjahansuchy@gmail.com>
2025-09-05 12:03:45 +0600

    add-remove

---
+++ Add-remove.js
@@ -0,0 +1,12 @@
+const tourPlace = ["Sajek","Rangamati","Cox's bazar"];
+console.log(tourPlace);
+
+tourPlace.push("St.Martine");
+console.log(tourPlace);
+
+tourPlace.push("Jaflong", "Bandarban"); 
+console.log(tourPlace);
+
+tourPlace.pop();
+console.log(tourPlace);
+
