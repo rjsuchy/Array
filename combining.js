e97b37d1657676da36f8ca242548332c7684fa0e
rjsuchy <rifatjahansuchy@gmail.com>
2025-09-05 12:25:20 +0600

    aaa

---
+++ combining.js
@@ -0,0 +1,7 @@
+const list1 = [ 2,4,6,8];
+const list2 = [1,3,5,7];
+
+console.log(list1);
+console.log(list2);
+
+console.log()
--- includes.js
+++ includes.js
@@ -4,3 +4,14 @@ console.log(Booklist.indexOf(99));
 
 console.log(Booklist.includes("al quran"));
 console.log(Booklist.indexOf("al quran"));
+
+if (Booklist.includes("javascript")){
+    console.log("No need to buy!");
+}
+    
+
+else {
+    console.log("No book found.Collect it");
+
+}
+    
