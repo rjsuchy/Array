7946f57935fec97021e038b2f2afd3f5bafd0005
rjsuchy <rifatjahansuchy@gmail.com>
2025-09-12 09:50:08 +0600

    aa

---
+++ Loops.js
@@ -0,0 +1,43 @@
+/*    const numbers =[3,6,9,12,18];
+   console.log(numbers);
+
+   for (const num of numbers){
+    console.log(num);
+   }
+
+   const fruits = [ "Mango", "BAnana", "Litchi", "Cherry" ];
+
+   for(const fruit of fruits){
+    
+    console.log("I will eat:");
+    console.log(fruit);
+   }
+ */
+
+   //----while loop------//
+   
+   
+let count = 0;
+
+while(count<=10){
+    console.log(count);
+    count++;
+
+}
+
+let num = 1;
+/* let sum = 0;
+while(num <= 10){
+    console.log(num);
+    sum= sum+num;
+    console.log('sum:', sum);
+    num++;
+} */
+
+while(num <= 10){
+    console.log(num);
+
+    if (num % 2 === 0){
+        console.log("Even:",num)
+    }
+     } 
