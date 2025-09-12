a2a1ae7600d7c9a1798219398c75af78e7d27e90
rjsuchy <rifatjahansuchy@gmail.com>
2025-09-12 11:12:08 +0600

    ff

---
+++ Strings-p/case.js
@@ -0,0 +1,14 @@
+const school = 'RAJ UK MODEL SCHOOL';
+
+console.log(school);
+
+const sub = 'chemistry';
+const book = 'Chemistry';
+
+if(sub === book ){
+    console.log('I am reading book');
+}
+
+else { 
+    console.log('nOT READING');}
+    
